e391f09bf1cb10971b4079a50f00b0c1ca88199c
jeffbukk00 <jeffbukk00@gmail.com>
2024-05-30 11:15:56 +0900

    build: separate environment of production from development

--- src/axios/constants/index.ts
+++ src/axios/constants/index.ts
@@ -1,1 +1,15 @@
-export const SERVER_HOST = import.meta.env.VITE_SERVER_HOST;
+const getServerHost = () => {
+  let SERVER_HOST;
+
+  const isDevMode = import.meta.env.DEV;
+  const isProdMode = import.meta.env.PROD;
+
+  if (isDevMode && !isProdMode)
+    SERVER_HOST = import.meta.env.VITE_SERVER_HOST_DEV;
+  if (!isDevMode && isProdMode)
+    SERVER_HOST = import.meta.env.VITE_SERVER_HOST_PROD;
+
+  return SERVER_HOST;
+};
+
+export const SERVER_HOST = getServerHost();
